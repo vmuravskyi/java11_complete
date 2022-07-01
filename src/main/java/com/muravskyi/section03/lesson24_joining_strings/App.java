bee4323729ad8e624693de94b8ae39cb1e8ce052
v.muravskyi <v.muravskyi@gmail.com>
2022-07-01 20:00:15 +0300

    save

---
+++ src/main/java/com/muravskyi/section03/lesson24_joining_strings/App.java
@@ -0,0 +1,11 @@
+package com.muravskyi.section03.lesson24_joining_strings;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
---
+++ src/main/java/com/muravskyi/section03/lesson29_user_input/App.java
@@ -0,0 +1,28 @@
+package com.muravskyi.section03.lesson29_user_input;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter value in fahrenheit: ");
+		double userInput = scanner.nextDouble();
+		
+		scanner.close();
+				
+		String result = String.format(
+			"The result of convertin %.2f fahrenheit to celcius is: %.2f", userInput, convertToCelsius(userInput)
+		);
+		System.out.println(result);
+	}
+
+	public static double convertToCelsius(double value) {
+		return Double.valueOf(
+			String.format("%.2f", ((value - 32) * (5.0 / 9.0)))
+		);
+	}
+
+}
