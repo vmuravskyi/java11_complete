76fc7b87940b27ea4f3941ca62ae29644e5a201c
vmuravskyi <v.muravskyi@connectwise.com>
2022-07-22 21:41:39 +0300

    save

---
+++ src/main/java/com/muravskyi/section26_midi/App.java
@@ -0,0 +1,29 @@
+package com.muravskyi.section26_midi;
+
+import javax.sound.midi.*;
+
+public class App {
+
+    public static void main(String[] args) throws MidiUnavailableException, InvalidMidiDataException, InterruptedException {
+
+        Receiver receiver = MidiSystem.getReceiver();
+
+        // create midi message1
+        ShortMessage message1 = new ShortMessage();
+        ShortMessage message2 = new ShortMessage();
+        ShortMessage message3= new ShortMessage();
+        message1.setMessage(ShortMessage.NOTE_ON, 0, 60, 127);
+        message2.setMessage(ShortMessage.NOTE_ON, 0, 64, 127);
+        message3.setMessage(ShortMessage.NOTE_ON, 0, 67, 127);
+
+        receiver.send(message1, -1);
+        receiver.send(message2, -1);
+        receiver.send(message3, -1);
+
+
+        Thread.sleep(3000);
+
+
+    }
+
+}
